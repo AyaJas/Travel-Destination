82d6c7e73766340633ea80fe5c22c39a1817b9fa
AyaJas <aya.hameedat@gmail.com>
2022-02-13 19:41:05 +0200

    Task16

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,25 @@
+import './Footer.css'
+
+function Footer() {
+
+    return (
+
+        <>
+
+            <h4>
+                aya jas © 2022 <br />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-twitter"></a>
+                <a href="#" class="fa fa-linkedin"></a>
+            </h4>
+
+        </>
+
+    );
+
+
+
+}
+
+export default Footer;
